c317ccea65b01342a0ec484253e30ad92128e874
Yamarrie <69699350+Yamarrie@users.noreply.github.com>
2020-08-15 12:22:54 +0100

    Adding Eclipse and updating space.cpp
    Just doing some more work on space.cpp and installing eclipse as an IDE

--- Space/space.cpp
+++ Space/space.cpp
@@ -1,7 +1,35 @@
-#include iostream
+#include <iostream>
 
 int main() {
 
+	double weight 
+	int nPlanet
 
+	std::cout << "Enter your weight on Earth: ";
+	std::cin >> weight;
+
+	std::cout << "\nPlanet options available:\n1. Venus\n2. Mars\n3. Jupiter\n4. Saturn\n5. Uranus\n6. Neptune\n";
+	std::cout << "Enter the number of the planet you would like to visit: ";
+    std::cin >> nPlanet;
+
+    switch(nPlanet); {
+
+    	case 1:
+    		pWeight = weight * 0.78;
+    		std::cout << "Your weight on Venus is: " << pWeight;
+    		break;
+
+    	case 2: 
+    		pWeight = weight * 0.39;
+    		std::cout << "Your weight on Mars is: " << pWeight;
+    		break;
+
+    	case 3:
+    		pWeight = weight * 2.65;
+    		std::cout << "Your weight on Jupiter is: " << pWeight
+
+    }
+
+    
 
 }
